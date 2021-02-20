a4305e0d885a57cbc7752913f97ff969831dbe8a
Rajat Kumar <rajatissar94@gmail.com>
2021-02-20 22:35:32 +0530

    json-server added

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('./src/db.json');
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 4001;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on ${PORT}`);
+});
